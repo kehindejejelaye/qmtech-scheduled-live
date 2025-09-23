d4c7fc336a9645d8d2df618f8c76802d8a6742e6
Kehinde Jejelaye <kehinde.jejelaye@fastcredit-ng.com>
2025-09-23 23:36:27 +0100

    add: add starter files

---
+++ src/main/java/com/qmtech/scheduledlive/QmtechScheduledLiveApplication.java
@@ -0,0 +1,13 @@
+package com.qmtech.scheduledlive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QmtechScheduledLiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QmtechScheduledLiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qmtech/scheduledlive/QmtechScheduledLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qmtech.scheduledlive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QmtechScheduledLiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
